f295a303f117476a5e7c86efc6f3de373276db04
mkarmark <mikarmar@microsoft.com>
2024-05-13 14:35:59 -0700

    Create GetRoles.cs

---
+++ dotnet-8/GetRoles.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Reflection;
+using System.Runtime.Versioning;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace StaticWebAppsEndToEndTesting.GetMessage
+{
+    public class GetMessage
+    {
+        [Function("GetRoles")]
+        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
+        {
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            var roles = new List<string>
+            {
+                "customRole"
+            };
+            string message = JsonConvert.SerializeObject(roles);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+            response.WriteString(message);
+            return response;
+        }
+    }
+}
